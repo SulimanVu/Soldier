665ad6f99b0613d32db72858ba2d9a621e50926e
Suliman <100429884+SulimanVu@users.noreply.github.com>
2022-07-12 13:53:20 +0300

    Написаны методы: стрелять, ранить, перезарядить

--- object-soldier.js
+++ object-soldier.js
@@ -1,7 +1,35 @@
     const soldier = {
         name: 'Ширвани',
-        health:  100,
-        gun: 'AK-47',
-        supplies: 240,
-        
+        health:  10,
+        gun: {
+            name: 'AK-47',
+            numCartridges: 0,
+        },
+        supplies: 1,
+        fire: function(){
+            if(this.gun.numCartridges>0){
+                this.gun.numCartridges = this.gun.numCartridges - 1;
+                console.log('бах-бах')
+            }else{
+                console.log('Обойма пуста. Перезарядитесь')
+                return 0
+            }
+        },
+        recharge: function(){
+            if(this.gun.numCartridges == 0){
+                console.log('перезарядка ...')
+                this.gun.numCartridges = 30;
+                this.supplies = this.supplies-1;
+            }else if(this.supplies == 0){
+                console.log('не осталось припасов')
+            }
+        },
+        hurt: function(){
+            this.health--
+            if(this.health == 0){
+                console.log('Ты проиграл')
+            }
+        }
     }
+    soldier.fire();
+    soldier.recharge();
